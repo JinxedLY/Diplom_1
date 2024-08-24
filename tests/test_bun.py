e502c6cee8c32739776aac2390c7cae00084b78d
Denis Kharitonov <manonov004@gmail.com>
2024-08-24 17:16:41 +0300

    fixed up test names

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -4,10 +4,10 @@ from stuff.test_data import Data
 
 
 class TestBuns:
-    def test_get_bun_name_success(self):
+    def test_get_bun_name_returns_bun_name(self):
         fresh_bun = Bun(Data.BUN_NAME, Data.BUN_PRICE)
         assert fresh_bun.get_name() == Data.BUN_NAME
 
-    def test_get_bun_price_success(self):
+    def test_get_bun_price_returns_bun_price(self):
         fresh_price = Bun(Data.BUN_NAME, Data.BUN_PRICE)
         assert fresh_price.get_price() == Data.BUN_PRICE
--- tests/test_burger.py
+++ tests/test_burger.py
@@ -1,12 +1,12 @@
 class TestBurger:
-    def test_set_buns_success(self):
+    def test_set_buns_sets_the_bun(self):
 
-    def test_add_ingredient_success(self):
+    def test_add_ingredient_add_ingredient(self):
 
-    def test_remove_ingredient_success(self):
+    def test_remove_ingredient_removes_ingredient(self):
 
-    def test_move_ingredient_success(self):
+    def test_move_ingredient_rearranges_ingredients(self):
 
-    def test_get_price_success(self):
+    def test_get_price_gets_price(self):
 
-    def test_get_receipt_success(self):
+    def test_get_receipt_gets_recipe(self):
