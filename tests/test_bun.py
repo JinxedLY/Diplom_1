79f4895b27e3eb057314fbe35849510623b8e30d
Denis Kharitonov <manonov004@gmail.com>
2024-08-24 13:49:02 +0300

    buns tested

---
+++ tests/test_bun.py
@@ -0,0 +1,12 @@
+import pytest
+from praktikum.bun import Bun
+from stuff.test_data import Data
+
+class TestBuns:
+    def test_get_bun_name_success(self):
+        fresh_bun = Bun(Data.BUN_NAME, Data.BUN_PRICE)
+        assert fresh_bun.get_name() == Data.BUN_NAME
+
+    def test_get_bun_price_success(self):
+        fresh_price = Bun(Data.BUN_NAME, Data.BUN_PRICE)
+        assert fresh_price.get_price() == Data.BUN_PRICE
--- tests/tests_bun.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from praktikum.bun import Bun
-
-def test_get_bun_name_success(bun_mock):
-
-
-def test_get_bun_price_success(bun_mock):
