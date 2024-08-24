34b5568a65bd536748e43fbf14b4c4097acce934
Denis Kharitonov <manonov004@gmail.com>
2024-08-24 13:40:39 +0300

    init test_data, revised mocks

--- conftest.py
+++ conftest.py
@@ -1,27 +1,28 @@
 import pytest
 from unittest.mock import Mock
+from stuff.test_data import Data
 from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
 
 
 @pytest.fixture
 def bun_mock():
     bun_mock = Mock()
-    bun_mock.get_price.return_value = 20.20
-    bun_mock.get_name.return_value = "bun_mock"
+    bun_mock.get_price.return_value = Data.BUN_PRICE
+    bun_mock.get_name.return_value = Data.BUN_NAME
     return bun_mock
 
 
 @pytest.fixture
 def sauce_mock():
     sauce_mock = Mock()
-    sauce_mock.get_price.return_value = 25.25
-    sauce_mock.get_name.return_value = "sauce_mock"
+    sauce_mock.get_price.return_value = Data.SAUCE_1_PRICE
+    sauce_mock.get_name.return_value = Data.SAUCE_1_NAME
     sauce_mock.get_type.return_value = INGREDIENT_TYPE_SAUCE
 
 
 @pytest.fixture
 def filling_mock():
     filling_mock = Mock()
-    filling_mock.get_price.return_value = 35
-    filling_mock.get_name.return_value = "filling_mock"
+    filling_mock.get_price.return_value = Data.FILLING_1_PRICE
+    filling_mock.get_name.return_value = Data.FILLING_1_NAME
     filling_mock.get_type.return_value = INGREDIENT_TYPE_FILLING
---
+++ tests/tests_bun.py
@@ -0,0 +1,7 @@
+import pytest
+from praktikum.bun import Bun
+
+def test_get_bun_name_success(bun_mock):
+
+
+def test_get_bun_price_success(bun_mock):
